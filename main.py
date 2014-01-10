f371cdb730ece2b4fd1110d45b53d2cb859fa4ef
Jeremy Young <jedayoung@gmail.com>
2014-01-09 17:59:26 -0800

    Eliminated redundant code.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from Tkinter import *
 
 class Display(Frame):
 
+
     def __init__(self, master=None):
         Frame.__init__(self, master)
         self.output = Label(self, text='0', bg='white', bd=5, \
@@ -10,6 +11,7 @@ class Display(Frame):
         self.initial = True
         self.seen_op = False
         self.ready_equals = False
+
 
     def append_num(self, num):
         if self.initial == True:
@@ -20,6 +22,7 @@ class Display(Frame):
                 self.ready_equals = True
             self.output['text'] += num
 
+
     def append_op(self, op):
         if self.initial == True:
             self.output['text'] = '0' 
@@ -27,17 +30,22 @@ class Display(Frame):
             self.output['text'] += ' ' + op + ' '  
             self.seen_op = True
 
+
     def get_text(self):
         return self.output['text']
+
 
     def set_text(self, new_text):
         self.output['text'] = new_text
 
+
     def set_initial(self, boolean):
         self.initial = boolean
  
+
     def get_seen_op(self):
         return self.seen_op
+
 
     def set_seen_op(self, boolean):
         self.seen_op = boolean
@@ -45,6 +53,7 @@ class Display(Frame):
 
 
 class Application(Frame):
+
 
     def __init__(self, display, master=None):
         Frame.__init__(self, master)
@@ -62,13 +71,14 @@ class Application(Frame):
         op_info = { '+': (3, 3), '-': (2, 3), '*': (1, 3), '/': (0, 3) }
         self.operators = {}
 
-        #Creates the number buttons.
+        #Creates the 10 number buttons.
         for i in range(10):
             button = Button(self, text=str(i), height=2, width=5)
             button['command'] = lambda i=i: self.append_num_general(str(i))
             r, c = num_positions[i][0], num_positions[i][1]
             button.grid(row=r, column=c)
 
+        #Creates the 4 operator buttons.
         for op,position in op_info.iteritems():
             button = Button(self, text=str(op), height=2, width=5)
             button['command'] = lambda op=op: self.append_op_general(op)
@@ -86,17 +96,19 @@ class Application(Frame):
         self.change_equals_state(DISABLED)
         self.change_ops_state(DISABLED)
 
+
     def append_num_general(self, num):
         if not self.display.get_seen_op():
             self.change_ops_state(ACTIVE)
         else:
             self.change_equals_state(ACTIVE)
-
         self.display.append_num(num) 
+
 
     def append_op_general(self, op):
         self.change_ops_state(DISABLED)
         self.display.append_op(op)
+
 
     def perform_op(self):
         args = self.display.get_text().split(' ') 
@@ -116,6 +128,7 @@ class Application(Frame):
 
         self.reset(result)
 
+
     def reset(self, new_text):
         self.display.set_text(new_text)
         self.display.set_initial(True)
@@ -123,9 +136,11 @@ class Application(Frame):
         self.change_ops_state(DISABLED)
         self.change_equals_state(DISABLED)
 
+
     def change_ops_state(self, state):
         for op_button in self.operators.values():
             op_button['state'] = state
+
 
     def change_equals_state(self, state):
         self.equals['state'] = state
